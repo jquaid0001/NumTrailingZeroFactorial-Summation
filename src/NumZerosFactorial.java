28f77c8eb0ebb11b356cb21ac0db6f35e2306a67
Josh Quaid <josh.quaid@gmail.com>
2022-07-19 16:15:58 -0500

    Final product. All tests passing.

--- src/NumZerosFactorial.java
+++ src/NumZerosFactorial.java
@@ -2,6 +2,17 @@ public class NumZerosFactorial {
 
     public static int zeros(int n) {
 
-        return -1;
+        // No need to do anything with this just return zero
+        if (n == 0) { return 0; }
+
+        // Get the result of log5 of n
+        double kMax = Math.floor(Math.log(n) / Math.log(5));
+
+        double numTrailingZeros = 0;
+        for (int k = 1; k <= kMax; k++) {
+            numTrailingZeros += Math.floor(n/Math.pow(5.0, k));
+        }
+
+        return (int) numTrailingZeros;
     }
 }
--- src/NumZerosFactorialTest.java
+++ src/NumZerosFactorialTest.java
@@ -4,12 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class NumZerosFactorialTest {
 
     @Test
-    void zeros() throws Exception {
-        assertEquals(NumZerosFactorial.zeros(0), 0);
-        assertEquals(NumZerosFactorial.zeros(6), 1);
-        assertEquals(NumZerosFactorial.zeros(14), 2);
-        assertEquals(NumZerosFactorial.zeros(12), 2);
-        assertEquals(NumZerosFactorial.zeros(6), 1);
-
+    void zeros() {
+        assertEquals(0, NumZerosFactorial.zeros(0));
+        assertEquals(1, NumZerosFactorial.zeros(6));
+        assertEquals(2, NumZerosFactorial.zeros(14));
+        assertEquals(2, NumZerosFactorial.zeros(12));
+        assertEquals(6, NumZerosFactorial.zeros(25));
+        assertEquals(28, NumZerosFactorial.zeros(121));
+        assertEquals(0, NumZerosFactorial.zeros(3));
+        assertEquals(1, NumZerosFactorial.zeros(5));
+        assertEquals(3, NumZerosFactorial.zeros(18));
+        assertEquals(9, NumZerosFactorial.zeros(43));
+        assertEquals(1270, NumZerosFactorial.zeros(5090));
+        assertEquals(3, NumZerosFactorial.zeros(18));
     }
 }
